687c993e21abdce4e5acd3cf75d8b22aa7be71fa
Mykola <nnv2006@ukr.net>
2024-03-22 14:53:45 +0200

    ADD: navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Links from './links/Links';
+
+function Navbar() {
+    return (
+        <div>
+            <idv>Logo</idv>
+            <div>
+                <Links />
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
