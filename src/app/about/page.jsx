b60be5f4267d82b9412c4c5a5a3f93c45efb216e
Mykola <nnv2006@ukr.net>
2024-03-28 22:01:18 +0200

    ADD: Image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 function AboutPage() {
-  return (
-    <div>
-      AboutPage
-    </div>
-  )
+    return (
+        <div>
+            <Image src="/about.png" alt="" width={500} height={500} />
+        </div>
+    );
 }
 
-export default AboutPage
+export default AboutPage;
