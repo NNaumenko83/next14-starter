bb5b7442459cda48a1a08d152b5e23fcf1f0c4cb
Mykola <nnv2006@ukr.net>
2024-03-29 23:04:25 +0200

    UPD: Contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styles from './contact.module.css';
+import Image from 'next/image';
+Image;
 
 function ContactPage() {
-  return (
-    <div>
-      ContactPage
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image className={styles.img} src="/contact.png" alt="" fill />
+            </div>
+            <div className={styles.formContainer}>
+                <form action="" className={styles.form}>
+                    <input type="text" placeholder="Name ans Surname" />
+                    <input type="email" placeholder="Email address" />
+                    <input type="tel" placeholder="Phone Number (Optional)" />
+                    <textarea
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="10"
+                        placeholder="Message"
+                    ></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
-export default ContactPage
+export default ContactPage;
