9afa12def102832a8f49d135be83a2a2d9794003
Mykola <nnv2006@ukr.net>
2024-03-31 22:36:33 +0300

    UPD: SinglePostPage

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import styles from './singlePost.module.css';
+import Image from 'next/image';
 
 function SinglePostPage() {
-  return (
-    <div>
-      SinglePostPage
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image
+                    className={styles.img}
+                    src="https://images.pexels.com/photos/17554347/pexels-photo-17554347/free-photo-of-taxis-in-front-of-palace-in-istanbul.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                    alt=""
+                    fill
+                />
+            </div>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Title</h1>
+                <div className={styles.detail}>
+                    <Image
+                        className={styles.avatar}
+                        src="https://images.pexels.com/photos/17554347/pexels-photo-17554347/free-photo-of-taxis-in-front-of-palace-in-istanbul.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                        alt=""
+                        fill
+                    />
+                    <div className={styles.detailtext}>
+                        <span className={styles.detailAuthor}>Authot</span>
+                        <span className={styles.detailValue}>
+                            Terry Jeferson
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default SinglePostPage
+export default SinglePostPage;
