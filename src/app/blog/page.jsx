5149b3175f17961248958b940b2f3c057aa56a72
Mykola <nnv2006@ukr.net>
2024-04-05 16:46:31 +0300

    fetch data without API

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import styles from './blog.module.css';
 import PostCard from '@/components/postCard/PostCard';
+import { getPosts } from '@/lib/data';
 
-const getData = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+// FETCH DATA WITH API
 
-    if (!response.ok) {
-        throw new Error('Something went wrong');
-    }
-    const data = await response.json();
-    return data;
-};
+// const getData = async () => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+//     if (!response.ok) {
+//         throw new Error('Something went wrong');
+//     }
+//     const data = await response.json();
+//     return data;
+// };
 
 async function BlogPage() {
-    const posts = await getData();
+    // FETCH DATA WITH API
+    // const posts = await getData();
+
+    // FETCH DATA WITHOUT API
+    const posts = await getPosts();
+
     console.log('posts:', posts);
 
     return (
--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styles from './postUser.module.css';
+import { getUser } from '@/lib/data';
 
-const getData = async id => {
-    const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${id}`,
-        { cache: 'no-store' }
-    );
+// const getData = async id => {
+//     const response = await fetch(
+//         `https://jsonplaceholder.typicode.com/users/${id}`,
+//         { cache: 'no-store' }
+//     );
 
-    if (!response.ok) {
-        throw new Error('Something went wrong');
-    }
-    const data = await response.json();
-    return data;
-};
+//     if (!response.ok) {
+//         throw new Error('Something went wrong');
+//     }
+//     const data = await response.json();
+//     return data;
+// };
 
 const User = async ({ userId }) => {
-    const user = await getData(userId);
+    // const user = await getData(userId);
+
+    const user = await getUser(userId);
     return (
         <div className={styles.container}>
             <span className={styles.title}>Author</span>
