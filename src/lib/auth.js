ee066b7333f9581ef577472dbaa5301adfb754f1
Mykola <nnv2006@ukr.net>
2024-04-19 09:03:30 +0300

    UPD: AuthUser

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth';
 // import GitHub from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
+import { connectToDb } from './utils';
+import { User } from './models';
 export const {
     handlers: { GET, POST },
     auth,
@@ -13,4 +15,27 @@ export const {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            console.log(user, account, 'profile', profile);
+            if (account.provider === 'google') {
+                connectToDb();
+                try {
+                    const user = await User.findOne({ email: profile.email });
+                    if (!user) {
+                        const newUser = new User({
+                            username: profile.name,
+                            email: profile.email,
+                            image: profile.picture,
+                        });
+                        await newUser.save();
+                    }
+                } catch (error) {
+                    console.log(error);
+                    return false;
+                }
+            }
+            return true;
+        },
+    },
 });
